340c011ee3a8a0def757d17251113d9f4222bb0c
Minju25Kim <48757517+minju25kim@users.noreply.github.com>
2022-09-23 22:50:12 +0100

    form and data is ready for database

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [expense, setExpense] = useState([]);
+  const [when, setWhen] = useState([]);
+  const [what, setWhat] = useState([]);
+
+  function handleData(e) {
+    e.preventDefault();
+    const { expense, when, what } = e.target;
+    setExpense(expense.value);
+    setWhen(when.value);
+    setWhat(what.value);
+  }
 
   return (
     <div className="App">
-      <h1>hi</h1>
+      <h1>Money time calculator</h1>
+      <form onSubmit={handleData}>
+        <label htmlFor="expense">Expense £ </label>
+        <input type="number" name="expense" id="expense"></input>
+        <br />
+        <label htmlFor="when">When: </label>
+        <input type="date" name="when" id="when"></input>
+        <br />
+        <label htmlFor="what">What: </label>
+        <input type="text" name="what" id="what"></input>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      {/* <div>
+        {expense || when || what ? (
+          <>
+            <span>{when}</span>
+            <span>{what}</span>
+            <span>{expense}</span>
+          </>
+        ) : null}
+      </div> */}
     </div>
   );
 }
